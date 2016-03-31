55fb5b97bc27305133bfea1e8ace30214e6e65c5
wang125309 <wang_125309@foxmail.com>
2016-03-31 18:51:14 +0800

    fix

--- setting/urls.py
+++ setting/urls.py
@@ -7,8 +7,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/userinfo/', 'portal.view.userView.user_info'),
-    url(r'^api/insertuser/', 'portal.view.userView.insert_user'),
-    url(r'^api/login/','portal.view.userView.login'),
-    url(r'^api/logout/','portal.view.userView.logout'),
+    url(r'^portal/submit/', 'portal.views.submit'),
 ]
