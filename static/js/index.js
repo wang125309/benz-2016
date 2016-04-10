218c85a5bb0707ddf5fb7890bd103486556bcc01
wang125309 <wang_125309@foxmail.com>
2016-04-10 17:55:11 +0800

    share

--- static/js/index.js
+++ static/js/index.js
@@ -2135,7 +2135,7 @@ $(function(){
             shareJson = {
                 link:"http://benz.importos.com",
                 imgUrl:"http://benz.importos.com/static/image/share-little-icon.jpg",
-                title:"天地既征途-2016 梅赛德斯-奔驰南区SUV征服之旅",
+                title:"天地即征途-2016 梅赛德斯-奔驰南区SUV征服之旅",
                 desc:"天地即征途——2016 梅赛德斯-奔驰SUV 即将在万众瞩目中强势来袭，作为南区该活动的前奏预热，将在南区10 大城市全线倾情展开梅赛德斯-奔驰南区SUV 征服之旅"
             };
 			wx.onMenuShareTimeline(shareJson);
@@ -2145,8 +2145,8 @@ $(function(){
             share();
         });
 		wx.error(function(res){
-			$.get("/wx/portal/update_access_token/",function(data){
-				$.post("/wx/portal/wxconfig/",{
+			$.get("/portal/update_access_token/",function(data){
+				$.post("/portal/wxconfig/",{
 					"url":location.href
 				},function(data){
 					wx.config(data);
