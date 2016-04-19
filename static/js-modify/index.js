154cd109bcc39a752a49a22d5c9d5798a4532932
wang125309 <wang_125309@foxmail.com>
2016-04-19 22:43:35 +0800

    x

--- static/js-modify/index.js
+++ static/js-modify/index.js
@@ -178,7 +178,7 @@ work = function(){
 		"3": [
             {"name":"厦门","pinyin":"xiamen","bus":[{"busname":"厦门东之星","address":"厦门市海沧区马青路1221号"},{"busname":"厦门东之星.星聚点","address":"厦门思明区体育路38号华强企业大楼一层之一"},{"busname":"厦门航星","address":"厦门高崎国际机场"}]},
 			{"name":"石狮","pinyin":"shishi","bus":[{"busname":"石狮大长江红星","address":"石狮市石泉路入口处奔驰大厦"}]},
-			{"name":"福州","pinyin":"fuzhou","bus":[{"busname":"福州东星","address":"福州市仓山区福峡路755号"},{"busname":"福州利之星","address":"福州市青口海峡汽车城（沈海高速兰圃出口往前200米）"},{"busname":"福建波士骏达","address":"福州市乌龙江南大道29号"},{"busname":"福建波士运达","address":"福州市福清市清荣大道中段（宏路高速往市区方向100米）"},{"busname":"福州鹏龙国戎","address":"福州市闽侯县荆溪镇永丰社区"}]},
+			{"name":"福州","pinyin":"fuzhou","bus":[{"busname":"福州东星","address":"福州市仓山区福峡路755号"},{"busname":"福州利之星","address":"福州市青口海峡汽车城（沈海高速兰圃出口往前200米）"},{"busname":"福建波士骏达","address":"福州市乌龙江南大道29号"},{"busname":"福州鹏龙国戎","address":"福州市闽侯县荆溪镇永丰社区"}]},
             {"name":"泉州","pinyin":"quanzhou","bus":[{"busname":"泉州闽星","address":"晋江世纪大道奔驰大厦（博物馆对面）"},{"busname":"泉州中升之星","address":"泉州清濛经济技术开发区迎宾大道"},{"busname":"泉州之星","address":"福建省泉州市丰泽区城华南路18号泉州之星奔驰"},{"busname":"泉州瑞星","address":"福建省泉州市鲤城区南环路99号瑞星大厦"},{"busname":"泉州中升星辉","address":"福建省泉州市南安市水头镇海联创业园DK31-B号地块"}]}
         ],
 		"4": [
